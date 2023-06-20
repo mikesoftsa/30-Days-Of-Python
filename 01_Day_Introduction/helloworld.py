e2226279a4139a0de960f9f0d2c9900bab4de4c2
Miguel Triana <Miguel Triana>
2023-06-19 21:14:10 -0500

    Day 1

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,51 @@
+# Day 1 - 30DaysOfPython Challenge
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
+
+# Checking data types
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Asabeneh'))
+print(type([1, 2, 3]))
+print(type({'name': 'Asabeneh'}))
+print(type({9.8, 3.14, 2.7}))
+print(type((9.8, 3.14, 2.7)))
+
+
+# Exercise: Level 1
+# 1. Check the python version you are using
+# python3 --version
+print('Python 3.8.10')
+
+# 2.Open the python interactive shell and do the following operations. The operands are 3 and 4.
+# addition(+)
+print(3 + 4)
+# subtraction(-)
+print(3 - 4)
+# multiplication(*)
+print(3 * 4)
+# modulus(%)
+print(3 % 4)
+# division(/)
+print(3 / 4)
+# exponential(**)
+print(3 ** 4)
+# floor division operator(//)
+print(3 // 4)
+
+
+# 3. Write strings on the python interactive shell. The strings are the following:
+# Your name
+print('Miguel')
+# Your family name
+print('Jorge')
+# Your country
+print('Ecuador')
+# I am enjoying 30 days of python
+print('I am enjoying 30 days of python')
